d06c5bba099deca04f0bd0a97afb5dd0fca65f5b
Sotiri Georgiou <tirigeorgiou@gmail.com>
2020-12-30 16:37:10 +0000

    Sorted fernet key

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 airflow initdb
 airflow webserver
+
+# Build FERNET KEY
+: "${AIRFLOW__CORE__FERNET_KEY:=${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}}"
